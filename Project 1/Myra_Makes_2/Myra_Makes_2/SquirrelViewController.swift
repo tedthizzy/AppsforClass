4ecab43a12bb2755a1b63b283965f69b6a201b20
tedthizzy <ted1508@gmail.com>
2017-02-28 10:20:42 -0700

    Myra Makes App
    DOES NOT RUN- only includes buttons, image views, view controllers, and
    segues. No functionality has been added. -Ted

---
+++ Project 1/Myra_Makes_2/Myra_Makes_2/SquirrelViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SquirrelViewController.swift
+//  Myra_Makes_2
+//
+//  Created by Frederick Thayer on 2/25/17.
+//  Copyright © 2017 FMT. All rights reserved.
+//
+
+import UIKit
+
+class SquirrelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
